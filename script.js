e7d8199d2cc486851ebc78d7f549b758b7059e72
Junaidanwar1 <anwarjunaid853@gmail.com>
2024-10-05 23:16:17 +0500

    adding js functionality

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const agecalculate = () => {
-     const today = new Date();
-     const inputdate = new Date(Document.getElementById("date-input").value);
+
+function calculateAge() {
+   const birthdate = document.getElementById("date-input").value;
+   if (birthdate === '') {
+       document.getElementById("display").innerText = "Please select a valid birthdate.";
+       return;
+   }
+
+   const birthDateObj = new Date(birthdate);
+   const today = new Date();
+   
+   let age = today.getFullYear() - birthDateObj.getFullYear();
+   const monthDifference = today.getMonth() - birthDateObj.getMonth();
+   const dayDifference = today.getDate() - birthDateObj.getDate();
+
+   if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+       age--;
+   }
+
+   document.getElementById("years").innerText = `${age}`;
+   document.getElementById("Month").innerText = `${monthDifference}`;
+   document.getElementById("Day").innerText = ` ${dayDifference}`;
 }
